4d03eeb812d55de2d21cb329141eaf8d9cf1cd69
VAMSIDHAR REDDY <vamsidhar1133@gmail.com>
2024-08-24 12:18:59 +0530

    Add files via upload

---
+++ Mad Libs Game (Python).py
@@ -0,0 +1,11 @@
+print("Welcome to the game!")
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input ("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + "are blue" )
+print("I love " + celebrity)
+
+print("Hope you Enjoyed")
